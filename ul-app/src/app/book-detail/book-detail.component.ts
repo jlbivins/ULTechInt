78c82ad738bb5bcee9ace6bb22fe93ff6952a8d3
jlb99 <bivins99@bellsouth.net>
2020-03-31 10:46:18 -0400

    initial checkin

---
+++ ul-app/src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
